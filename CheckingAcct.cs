9893a79a516a93b52898a32cb786280c055b6746
Bill Hice <billhice@gmail.com>
2021-08-20 14:59:03 -0500

    Add project files.

---
+++ CheckingAcct.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgChallenge
+{
+    class CheckingAcct 
+    {
+        private string _ownerFirstName = "";
+        private string _ownerLastName = "";
+        private decimal _balance = 0.0m;
+        private int _withdrawCounter =0;
+
+        public  string AccountOwner;
+        public string OwnerFirstName { get { return _ownerFirstName; } set { _ownerFirstName = value; } }
+        public string OwnerLastName { get { return _ownerLastName; } set { _ownerLastName = value; } }
+        public decimal Balance { get { return _balance; } set { _balance = value; } }
+        
+        public void Deposit (decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw( decimal amount)
+        {
+            
+            Balance -= amount;
+
+        }
+        
+        public CheckingAcct(string ownersFirstName, string ownersLastName, decimal balance)
+        {
+            this.OwnerFirstName = ownersFirstName;
+            this.OwnerLastName = ownersLastName;
+            this.Balance = balance;
+            AccountOwner = OwnerFirstName + " " + OwnerLastName;
+        }
+            
+    }
+}
---
+++ SavingsAcct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgChallenge
+{
+    class SavingsAcct : CheckingAcct
+    {
+        
+        private decimal _interest = 0.025m;
+        private int _count = 0;
+
+        public SavingsAcct(string OwnerFirstName, string OwnerSecondName, decimal Interest, decimal Balance) : base(OwnerFirstName, OwnerSecondName, Balance)
+        {
+            
+        }
+
+        public void ApplyInterest()
+        {
+            Balance *= 1 + _interest;
+        }
+
+        public new void Withdraw(decimal amount)
+        {
+            _count++;
+        }
+
+      
+
+    }
+}
